a3b05bdfc839dd21c66d9d5ab10e69f1b485c5d3
Nasif006 <md.nasifiqbal006@gmail.com>
2025-06-30 12:08:04 +0600

    featured all books connected to shop.php

--- LibraryFeaturedBooks.php
+++ LibraryFeaturedBooks.php
@@ -48,7 +48,7 @@
 			<div class="col-md-12">
 
 				<div class="btn-wrap align-right">
-					<a href="#" class="btn-accent-arrow">View all products <i class="icon icon-ns-arrow-right"></i></a>
+					<a href="LibraryShop.php" class="btn-accent-arrow">View all products <i class="icon icon-ns-arrow-right"></i></a>
 				</div>
 
 			</div>
